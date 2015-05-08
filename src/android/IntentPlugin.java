3f9b6f97a63078f7f8f1960e1979bed4d12f4648
Alexander Quast <alexander.quast@open-xchange.com>
2015-05-08 10:31:53 +0200

    finished first working version

--- src/android/IntentPlugin.java
+++ src/android/IntentPlugin.java
@@ -1,22 +1,37 @@
-package com.openexchange.cordova.IntentPlugin;
+package com.openexchange.cordova.intentplugin;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 public class IntentPlugin extends CordovaPlugin{
-	private final String TAG = "INTENTPLUGIN";
+    private final String TAG = "INTENT_PLUGIN";
 
-	@Override
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getIntentData")) {
-        	Intent i = this.cordova.getActivity().getIntent();
-        	Log.d(TAG, "Got intent " + i.getAction());
+            PluginResult.Status status = PluginResult.Status.OK;
+            Intent i = this.cordova.getActivity().getIntent();
+            Log.d(TAG, "Got intent " + i.getAction());
+            String intentAction = i.getAction();
+            String intentData = i.getDataString();
+            JSONObject result = new JSONObject();
+            Log.d(TAG, "Got intent data" + intentData);
+
+            if (intentAction != null) {
+                result.put("action", intentAction);
+            }
+            if (intentData != null) {
+                result.put("data", intentData);
+            }
+            callbackContext.sendPluginResult(new PluginResult(status, result));
             return true;
         }
         return false;
--- www/IntentPlugin.js
+++ www/IntentPlugin.js
@@ -1,18 +1,7 @@
 var IntentPlugin = {
-    foo: function () {
-        console.log('foo!');
-    },
-    test: function (cb, ecb) {
+
+    get: function (cb, ecb) {
         cordova.exec(cb, ecb, 'IntentPlugin', 'getIntentData', []);
-    },
-    get: function () {
-        var d = $.Deferred();
-        cordova.exec(function (data) {
-            d.resolve(data);
-        }, function (error) {
-            d.reject('error', error);
-        }, 'IntentPlugin', 'getIntentData', []);
-        return d;
     }
 };
 
