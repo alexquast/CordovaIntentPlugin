3cb1e5df08af219bfdb3f591487f9237e0653808
Alexander Quast <alexander.quast@open-xchange.com>
2015-05-15 12:15:08 +0200

    Added multi send intent

--- src/android/IntentPlugin.java
+++ src/android/IntentPlugin.java
@@ -1,5 +1,8 @@
 package com.openexchange.cordova.intentplugin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -9,6 +12,9 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 
 public class IntentPlugin extends CordovaPlugin{
@@ -22,19 +28,50 @@ public class IntentPlugin extends CordovaPlugin{
             Log.d(TAG, "Got intent " + i.getAction());
             String intentAction = i.getAction();
             String intentData = i.getDataString();
+            //Bundle extras = i.getExtras();
             JSONObject result = new JSONObject();
             Log.d(TAG, "Got intent data" + intentData);
+
 
             if (intentAction != null) {
                 result.put("action", intentAction);
             }
+            if (intentAction == "android.intent.action.SEND") {
+                Uri fileUri = (Uri) i.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (fileUri != null) {
+                    Log.d(TAG, "Image URI" + fileUri.toString());
+                    result.put("uri", fileUri);
+                }
+            }
+            if (intentAction == "android.intent.action.SEND_MULTIPLE") {
+                ArrayList<Uri> imageUris = i.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+
+
+                if (imageUris != null) {
+                    JSONArray a = new JSONArray();
+
+                    for (Uri uri : imageUris) {
+                        a.put(uri.toString());
+                        Log.d(TAG, "eine url ist " + uri.toString());
+                    }
+                    result.put("uri", a);
+                }
+            }
             if (intentData != null) {
                 result.put("data", intentData);
             }
+
             callbackContext.sendPluginResult(new PluginResult(status, result));
+            // clear the intent to avoid double use of the same intent
+            clearIntent(i);
+
             return true;
         }
         return false;
     }
 
+    public void clearIntent(Intent i) {
+        i.setAction("");
+        i.setData(null);
+    }
 }
